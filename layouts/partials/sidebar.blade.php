05692dcb9580ff96c3e1f6dffe7da3b659007444
marco76tv <marco.sottana@gmail.com>
2018-11-16 14:21:24 +0100

    template amp_simpleblog

--- layouts/partials/sidebar.blade.php
+++ layouts/partials/sidebar.blade.php
@@ -1,54 +1,54 @@
 <amp-sidebar id="penci_sidebar" class="mobile-sidebar" layout="nodisplay" side="left">
-			<button id="close-sidebar-nav" on="tap:penci_sidebar.close" class="ampstart-btn caps m2"><i class="fa fa-close"></i></button>
-			<div id="sidebar-nav-logo">
-				<a href="http://pennews.pencidesign.com/amp"
-					class="sidebar-branding penci-amp-site-icon sidebar-image-logo ">
-					<amp-img src="http://max.pennews.pencidesign.com/wp-content/uploads/2017/11/cropped-logo.png" alt="PenNews - The Best News &amp; Magazine WordPress Themes" height="50" width="231"></amp-img>
-				</a>
-			</div>
-			<div class="header-social sidebar-nav-social">
-				<div class="inner-header-social">
-					<a class="social-media-item socail_media__facebook" target="_blank" href="#" title="Facebook" rel="noopener">
-						<span class="socail-media-item__content">
-							<i class="fa fa-facebook"></i>
-							<span class="social_title screen-reader-text">Facebook</span>
-						</span>
-					</a>
-					<a class="social-media-item socail_media__twitter" target="_blank" href="#" title="Twitter" rel="noopener">
-						<span class="socail-media-item__content">
-							<i class="fa fa-twitter"></i>
-							<span class="social_title screen-reader-text">Twitter</span>
-						</span>
-					</a>
-					<a class="social-media-item socail_media__instagram" target="_blank" href="#" title="Instagram" rel="noopener">
-						<span class="socail-media-item__content">
-							<i class="fa fa-instagram"></i>
-							<span class="social_title screen-reader-text">Instagram</span>
-						</span>
-					</a>
-					<a class="social-media-item socail_media__pinterest" target="_blank" href="#" title="Pinterest" rel="noopener">
-						<span class="socail-media-item__content"><i class="fa fa-pinterest"></i>
-							<span class="social_title screen-reader-text">Pinterest</span>
-						</span>
-					</a>
-					<a class="social-media-item socail_media__google" target="_blank" href="#" title="Google" rel="noopener">
-						<span class="socail-media-item__content">
-							<i class="fa fa-google-plus"></i>
-							<span class="social_title screen-reader-text">Google</span>
-						</span>
-					</a>
-					<a class="social-media-item socail_media__youtube" target="_blank" href="#" title="Youtube" rel="noopener">
-						<span class="socail-media-item__content">
-							<i class="fa fa-youtube-play"></i>
-							<span class="social_title screen-reader-text">Youtube</span>
-						</span>
-					</a>		
-				</div>
-			</div>
-			<nav id="primary-menu-mobile" itemscope itemtype="http://schema.org/SiteNavigationElement" class="mobile-navigation primary-menu-mobile penci-amp-menu">
-				<li id="menu-item-3415" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item menu-item-3415"><a href="/amp/">Home</a></li>
-				<li id="menu-item-3416" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-3416"><a href="http://pennews.pencidesign.com/amp/category/life-style/fashion/">Fashion</a></li>
-				<li id="menu-item-3417" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-3417"><a href="http://pennews.pencidesign.com/amp/category/life-style/food/">Food</a></li>
-				<li id="menu-item-3418" class="menu-item menu-item-type-post_type menu-item-object-page current_page_parent menu-item-3418"><a href="http://pennews.pencidesign.com/amp/blog/">Blog</a></li>
-			</nav>
-		</amp-sidebar>
+	<button id="close-sidebar-nav" on="tap:penci_sidebar.close" class="ampstart-btn caps m2"><i class="fa fa-close"></i></button>
+	<div id="sidebar-nav-logo">
+		<a href="http://pennews.pencidesign.com/amp"
+			class="sidebar-branding penci-amp-site-icon sidebar-image-logo ">
+			<amp-img src="http://max.pennews.pencidesign.com/wp-content/uploads/2017/11/cropped-logo.png" alt="PenNews - The Best News &amp; Magazine WordPress Themes" height="50" width="231"></amp-img>
+		</a>
+	</div>
+	<div class="header-social sidebar-nav-social">
+		<div class="inner-header-social">
+			<a class="social-media-item socail_media__facebook" target="_blank" href="#" title="Facebook" rel="noopener">
+				<span class="socail-media-item__content">
+					<i class="fa fa-facebook"></i>
+					<span class="social_title screen-reader-text">Facebook</span>
+				</span>
+			</a>
+			<a class="social-media-item socail_media__twitter" target="_blank" href="#" title="Twitter" rel="noopener">
+				<span class="socail-media-item__content">
+					<i class="fa fa-twitter"></i>
+					<span class="social_title screen-reader-text">Twitter</span>
+				</span>
+			</a>
+			<a class="social-media-item socail_media__instagram" target="_blank" href="#" title="Instagram" rel="noopener">
+				<span class="socail-media-item__content">
+					<i class="fa fa-instagram"></i>
+					<span class="social_title screen-reader-text">Instagram</span>
+				</span>
+			</a>
+			<a class="social-media-item socail_media__pinterest" target="_blank" href="#" title="Pinterest" rel="noopener">
+				<span class="socail-media-item__content"><i class="fa fa-pinterest"></i>
+					<span class="social_title screen-reader-text">Pinterest</span>
+				</span>
+			</a>
+			<a class="social-media-item socail_media__google" target="_blank" href="#" title="Google" rel="noopener">
+				<span class="socail-media-item__content">
+					<i class="fa fa-google-plus"></i>
+					<span class="social_title screen-reader-text">Google</span>
+				</span>
+			</a>
+			<a class="social-media-item socail_media__youtube" target="_blank" href="#" title="Youtube" rel="noopener">
+				<span class="socail-media-item__content">
+					<i class="fa fa-youtube-play"></i>
+					<span class="social_title screen-reader-text">Youtube</span>
+				</span>
+			</a>		
+		</div>
+	</div>
+	<nav id="primary-menu-mobile" itemscope itemtype="http://schema.org/SiteNavigationElement" class="mobile-navigation primary-menu-mobile penci-amp-menu">
+		<li id="menu-item-3415" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item menu-item-3415"><a href="/amp/">Home</a></li>
+		<li id="menu-item-3416" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-3416"><a href="http://pennews.pencidesign.com/amp/category/life-style/fashion/">Fashion</a></li>
+		<li id="menu-item-3417" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-3417"><a href="http://pennews.pencidesign.com/amp/category/life-style/food/">Food</a></li>
+		<li id="menu-item-3418" class="menu-item menu-item-type-post_type menu-item-object-page current_page_parent menu-item-3418"><a href="http://pennews.pencidesign.com/amp/blog/">Blog</a></li>
+	</nav>
+</amp-sidebar>
